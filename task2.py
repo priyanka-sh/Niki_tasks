b16734af042533cb23ebaa72e6d9ed37ae4f31ad
priyanka-sh <priyanka.sharma1793@gmail.com>
2018-01-20 11:53:30 +0530

    error handling in case of out of range numbers

--- task2.py
+++ task2.py
@@ -6,12 +6,13 @@ def convert(A):
             A /= 26
             ret += chr(temp+ord('A'))
         return ret[::-1]
-
+flag = 0
 A = int(raw_input("Enter number1: "))
 B = int(raw_input("Enter number2: "))
 if(A>B or A<1 or  B<1 or  A>676 or B>676):
 	print "Enter valid set of numbers"
+	flag=1
 
-
-for i in range(A,B+1):
-	print "The "+str(i)+"th column is",convert(i)
+if flag==0:
+	for i in range(A,B+1):
+		print "The "+str(i)+"th column is",convert(i)
